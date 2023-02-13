afe0eda4cd0f9d5488fc1aa17db72031cbec9ca9
Camillar <camillacrew@mail.ru>
2023-02-13 22:50:35 +0400

    Добавили папку Exersice_3

---
+++ Exersice_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 1)
+{
+    Console.WriteLine($"Число ({number}) является: НЕЧЕТНЫМ");
+}
+else
+{
+    Console.WriteLine($"Число ({number}) является: ЧЕТНЫМ");
+}
