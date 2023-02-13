6072ab64106098d843176e4a454100d729ee2aea
Camillar <camillacrew@mail.ru>
2023-02-13 21:49:23 +0400

    Создали папку Exercise 2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 3 числа : ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = Convert.ToInt32(Console.ReadLine());
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+
+if (number_2 > max)
+{
+    max = number_2;
+}
+
+if (number_3 > max)
+{
+    max = number_3;
+}
+Console.WriteLine($"Наибольшее из введенных чисел ({max})");
